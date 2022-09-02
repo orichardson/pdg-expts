d0b5074ca4c096f78a1a41e86bdd03660e10d3e7
Oliver Richardson <oli@cs.cornell.edu>
2022-09-02 18:19:19 -0400

    ...

---
+++ code/expts/inference/data_collection.py
@@ -0,0 +1,45 @@
+"""
+Given a PDG, collet 
+"""
+
+
+#%%
+
+from pdg.pdg import PDG
+from pdg.store import TensorLibrary
+from pdg.rv import Variable as Var
+from pdg.dist import CPT, RawJointDist as RJD 
+
+def collect_data(id:str,  M:PDG,  store:TensorLibrary=None):
+    """
+    run a bunch of algorithms to do inference on the given PDG, and
+    store the results in the given tensorlibrary (if given).
+    Returns a tensor library of results.
+    """
+    
+    if store == None:
+        store = TensorLibrary()
+        
+    ## 1 ##  --- 
+    store.copy
+
+    ## 2 ##  --- 
+    ## 3 ##  --- torch optimization (Adam)
+    ## 4 ##  --- torch optimization (LBFGS)
+    ## 5 ##  --- interior point cvx optimization
+    ## 6 ##  --- LIR
+
+
+def colect_data(id:str, bn, store):
+    pass
+
+#%%
+# %cd ../..
+# %pwd
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+from pdg.lib.smoking import M 
+
+M.optimize_score()
