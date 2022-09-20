d22069888e9acae13e668b918a9e5fa6d25a0425
Oliver Richardson <oli@cs.cornell.edu>
2022-09-19 22:24:16 -0400

    data collection

--- code/expts/inference/data_collection.py
+++ code/expts/inference/data_collection.py
@@ -2,36 +2,77 @@
 Given a PDG, collet 
 """
 
-import logging
+import numpy as np
 
-
-
-from pgmpy.inference import ExactInference
+from pgmpy.inference import BeliefPropagation
+from pgmpy.utils import get_example_model
 
 from pdg.pdg import PDG
 from pdg.store import TensorLibrary
 from pdg.rv import Variable as Var
 from pdg.dist import CPT, RawJointDist as RJD 
 
-
 from pdg.alg import interior_pt as ip
 from pdg.alg import torch_opt
 
 
-
-example_bn_names =  [ 
-	"asia", "cancer", "earthquake", "sachs", "survey", "alarm", "barley", "child",
-    "insurance", "mildew", "water", "hailfinder", "hepar2", "win95pts", "andes", "diabetes",
-	"link", "munin1", "munin2", "munin3", "munin4", "pathfinder", "pigs", "munin" ]
-
-
-
-# import logging
+## TIMING / LOGGING UTILS
+import psutil
+# from multiprocessing import Process
+import multiprocessing as multiproc
+import time
+import pickle
+import logging
 # logging.basicConfig(format='%(asctime)s %(message)s', 
 # 	filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 
-def collect_inference_data_for(M:PDG,  store:TensorLibrary=None):
+
+def wrap(fn, return_bin, fname):
+	def fn_wrapped(*args, **kwargs):
+		init_time = time.time()
+		rslt = fn(*args, **kwargs)
+		total_time = time.time() - init_time
+
+		with open(fname+'.pickle', 'wb') as f:
+			pickle.dump(rslt, f)
+
+		return_bin.send({'time' : total_time})
+		return_bin.send(rslt)
+	
+	return fn_wrapped
+
+
+
+def glog(fname, func, *args, **kwargs):
+	recver, sender = multiproc.Pipe(False) #
+
+	p = multiproc.Process(target=wrap(func, sender, fname), args=args, kwargs=kwargs)
+	psu_p = psutil.Process(p.pid)
+
+	max_mem = 0
+	sleep_time = 1E-4
+
+	p.start()
+	sender.close()
+
+	while not recver.poll():
+		max_mem = max(max_mem, psu_p.memory_info().rss)
+		time.sleep(sleep_time)
+		sleep_time *= 1.5
+		print(psu_p.memory_info())
+
+	total_time = recver.get()['time']
+	rslt = recver.get()
+
+	return dict(
+		total_time = total_time,
+		max_mem = max_mem,
+		result = rslt,
+	)
+
+
+def collect_inference_data_for(idstr: str, M:PDG,  store:TensorLibrary=None):
 	"""
 	run a bunch of algorithms to do inference on the given PDG, 
 	write them to a file, and return a tensor library of results.
@@ -39,6 +80,14 @@ def collect_inference_data_for(M:PDG,  store:TensorLibrary=None):
 	
 	if store == None:
 		store = TensorLibrary()
+
+
+	stats = dict(
+		n_vars = len(M.varlist),
+		n_worlds = np.prod(M.dshape),
+		n_params = sum(p.size for p in M.edges('P')),
+		n_edges = len(M.Ed)
+	)
 		
 
 	# for each optimization, log:
@@ -46,18 +95,26 @@ def collect_inference_data_for(M:PDG,  store:TensorLibrary=None):
 	#  - memory taken
 	#  - training curve (if available): loss over time
 	#  - (Inc, Idef) of final product
-	# 
-	with open(idstr + ".log", 'w') as f:
-
-		mu1 = ip.cvx_opt_clusters(M, also_idef=False)
-		mu2 = ip.cvx_opt_clusters(M, also_idef=True)
 
 
-		f.write()
-		f.flush()
+	def log(idstr, method, *args, **kwargs):
+		dist = glog(idstr, method, M, *args, **kwargs)
+		inc = M.Inc(dist).real
+		idef = M.IDef(dist)
+
+		store(*args,inc=inc,idef=idef,
+			**stats, **kwargs).set(dist)
+
+	log(idstr+".ip.-idef", ip.cvx_opt_joint, also_idef=False)
+	log(idstr+".ip.+idef", ip.cvx_opt_joint, also_idef=True)
+
+	for gamma in [1E-12, 1E-8, 1E-4, 1E-2, 1]:
+		for ozrname in ['adam', "lbfgs", "asgd"]:
+			log(idstr+".torch.gamma%.0e"%gamma, 
+				torch_opt.opt_dist,
+				gamma=gamma, optimizer=ozrname)
+
 		
-	## 1 ##  --- 
-	## 2 ##  --- 
 	## 3 ##  --- torch optimization (Adam)
 	## 4 ##  --- torch optimization (LBFGS)
 	## 5 ##  --- interior point cvx optimization
@@ -77,16 +134,23 @@ def colect_data(id:str, bn, store):
 
 
 
-
-#%%
-
+example_bn_names =  [ 
+	"asia", "cancer", "earthquake", "sachs", "survey", "alarm", "barley", "child",
+	# "insurance", "mildew", "water", "hailfinder", "hepar2", "win95pts", "andes", "diabetes",
+	# "link", "munin1", "munin2", "munin3", "munin4", "pathfinder", "pigs", "munin" 
+]
 
 
 if __name__ == '__main__':
-	from pgmpy.readwrite import BIFReader
+	store = TensorLibrary()
 
-	reader = BIFReader("alarm.bif")
+	for bn_name in example_bn_names:
+		bn = get_example_model(bn_name)
+		bp = BeliefPropagation(bn)
+		glog(bn_name+"-as-FG.bp", bp.calibrate)
 
 
-# from pdg.lib.smoking import M 
-# M.optimize_score()
+		pdg = PDG.from_BN(bn)
+		collect_inference_data_for(bn_name+"-as-pdg", pdg, store)
+
+
