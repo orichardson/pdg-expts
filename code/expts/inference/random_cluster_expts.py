1c82dc066d27b1ca17f4b434add759c87dcb5caf
Oliver Richardson <oli@cs.cornell.edu>
2022-10-13 04:52:52 -0400

    better cluster trees

--- code/expts/inference/random_cluster_expts.py
+++ code/expts/inference/random_cluster_expts.py
@@ -150,9 +150,10 @@ try:
 
 		while len(pdg.edgedata) < m:
 			# c1,c2 = random.choice(list(ctree.edges()))
-			c1 = random.choice(list(ctree.nodes()))
-			c2 = random.choice(list(ctree[c1]))
-			options = [pdg.varlist[i] for i in set(c1) | set(c2)]
+			# c1 = random.choice(list(ctree.nodes()))
+			# c2 = random.choice(list(ctree[c1]))
+			# options = [pdg.varlist[i] for i in set(c1) | set(c2)]
+			options = random.choice(list(ctree.nodes()))
 
 			try:
 				src = random.sample(options, k=random.randint(*args.src_range))
